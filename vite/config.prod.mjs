48c9b00ad540a0cd04116bb312b2d2917bb3ccfa
Filip Cihak <cihakfil@fit.cvut.cz>
2026-06-06 01:34:58 +0200

    Add action for deploying client to GitHub Pages

--- vite/config.prod.mjs
+++ vite/config.prod.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 export default defineConfig({
-    base: './',
+    base: '/magocracy/',
     logLevel: 'warn',
     build: {
         rollupOptions: {
