4bd1433484520bf2acd54d063a56abedd8141b84
Filip Cihak <cihakfil@fit.cvut.cz>
2026-06-06 02:19:29 +0200

    Change vite prod base to default

--- vite/config.prod.mjs
+++ vite/config.prod.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 export default defineConfig({
-    base: '/magocracy/',
     logLevel: 'warn',
     build: {
         rollupOptions: {
